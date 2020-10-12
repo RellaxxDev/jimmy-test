a2b06fecff95e58dc5f1cf7ddfc0de0d750efad4
Vitaly Solovyov <vitaly.solovyov@tibbo.com>
2020-10-12 16:47:31 +0300

    simplify api

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,42 +1,33 @@
 import { IManufacture } from "../types/IManufacture";
-import {IModel} from "../types/IModel";
+import { IModel } from "../types/IModel";
 
 const API_LINK = "https://vpic.nhtsa.dot.gov/api/vehicles";
 
-const manufacturesNormalization =
-  ({ Mfr_ID: id, Mfr_CommonName: name, Country: country }:
-     { Mfr_ID: string; Mfr_CommonName: string, Country: string; }): IManufacture => ({ id, name, country });
+const manufacturesNormalization = ({ Mfr_ID: id, Mfr_CommonName: name, Country: country }: { Mfr_ID: string; Mfr_CommonName: string; Country: string }): IManufacture => ({ id, name, country });
 
-const modelsNormalization =
-  ({ VehicleTypeId: id, VehicleTypeName: name }:
-     { VehicleTypeId: string; VehicleTypeName: string }): { id: string; name: string } => ({ id, name });
+const modelsNormalization = ({ VehicleTypeId: id, VehicleTypeName: name }: { VehicleTypeId: string; VehicleTypeName: string }): { id: string; name: string } => ({ id, name });
 
-const makersNormalization =
-  ({ Make_ID: id, Make_Name: name }:
-     { Make_ID: string; Make_Name: string }): {id: string; name: string} => ({ id, name });
+const makersNormalization = ({ Make_ID: id, Make_Name: name }: { Make_ID: string; Make_Name: string }): { id: string; name: string } => ({ id, name });
 
 export const getManufactures = async (): Promise<IManufacture[]> => {
   const request = new Request(`${API_LINK}/GetAllManufacturers?format=json`, {
-    method: "GET"
+    method: "GET",
   });
-  const { Results } = await fetch(request).then((res) => res.json());
-  return Results.map(manufacturesNormalization);
+  return get(request, manufacturesNormalization);
 };
 
 const getModelsForMakeId = async (id: string): Promise<IModel[]> => {
-  const requestMakers = new Request(`${API_LINK}/GetVehicleTypesForMakeId/${id}?format=json`, {
+  const request = new Request(`${API_LINK}/GetVehicleTypesForMakeId/${id}?format=json`, {
     method: "GET",
   });
-  const { Results } = await fetch(requestMakers).then((res) => res.json());
-  return Results.map(modelsNormalization);
+  return get(request, modelsNormalization);
 };
 
 export const getModels = async (id: string): Promise<IModel[]> => {
-  const requestMakers = new Request(`${API_LINK}/GetMakeForManufacturer/${id}?format=json`, {
+  const request = new Request(`${API_LINK}/GetMakeForManufacturer/${id}?format=json`, {
     method: "GET",
   });
-  const { Results } = await fetch(requestMakers).then((res) => res.json());
-  const makers = Results.map(makersNormalization);
+  const makers = await get(request, makersNormalization);
 
   const promises: Promise<IModel[]>[] = [];
   for (const makerObject of makers) {
@@ -44,13 +35,26 @@ export const getModels = async (id: string): Promise<IModel[]> => {
     promises.push(getModelsForMakeId(makeId));
   }
 
-  return await Promise
-    .all(promises)
-    .then(response => {
-      const result: IModel[] = [];
-      for (let i = response.length - 1; i >= 0; i--) {
-        result.push(...response[i]);
+  return await Promise.all(promises).then((response) => {
+    const result: IModel[] = [];
+    for (let i = response.length - 1; i >= 0; i--) {
+      result.push(...response[i]);
+    }
+    return result;
+  });
+};
+
+const get = async <T>(request: Request, normalizeFunction?: (...args: any[]) => T): Promise<T[]> => {
+  const { Results } = await fetch(request)
+    .then((result) => {
+      if (!result.ok) {
+        throw Error(result.statusText);
       }
-      return result;
+      return result.json();
+    })
+    .catch((er) => {
+      console.error(er);
+      return { Results: [] };
     });
+  return Results.map(normalizeFunction);
 };
